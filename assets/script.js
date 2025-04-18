70048e81708da3a1fa95e36d08cd009c9b199255
janspecter <jrs2021.98@gmail.com>
2025-04-18 17:41:06 -0300

    Adicionados

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+  function abrirPDFs() {
+    setTimeout(() => {
+        window.open('public/acessibilidade em museus.pdf', '_blank');
+    }, 0)
+
+    setTimeout(() => {
+        window.open('public/curso-para fazer uma exposição.pdf', '_blank');
+    }, 100)
+  }
+
+  let show = true;
+  const menuContent = document.querySelector('.content');
+  const menuToggle = menuContent.querySelector('.menu-toggle');
+  
+  menuToggle.addEventListener("click", () => {
+  
+      menuContent.classList.toggle('on', show);
+      show = !show;
+  
+  });
